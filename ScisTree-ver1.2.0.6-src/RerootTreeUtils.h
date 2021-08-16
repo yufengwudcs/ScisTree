cca76776ac0fd6fd6f2e186b540066d5da0eff9b
Yufeng Wu <yufengwudcs@gmail.com>
2021-08-16 15:17:06 -0400

    Add files via upload

---
+++ ScisTree-ver1.2.0.6-src/RerootTreeUtils.h
@@ -0,0 +1,17 @@
+//
+//  RerootTreeUtils.h
+//  
+//
+//  Created by Yufeng Wu on 8/10/18.
+//
+//
+
+#ifndef RerootTreeUtils_h
+#define RerootTreeUtils_h
+
+#include <string>
+
+std::string ReRootTreeNewick( char *nwFile, char *taxaNewRoot );
+
+
+#endif /* RerootTreeUtils_h */
